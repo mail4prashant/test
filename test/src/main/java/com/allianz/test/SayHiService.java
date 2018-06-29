0b9f49c8ce1ba55a951b9cd4f270ef3cc7dfd797
Prashant Pandey <mail4prashant@gmail.com>
2018-06-29 11:14:41 +0200

    let sonar success

--- test/src/main/java/com/allianz/test/SayHiService.java
+++ test/src/main/java/com/allianz/test/SayHiService.java
@@ -7,9 +7,6 @@ public class SayHiService {
 	private final static String HI = "Hi";
 	
 	public String sayHi(String name) {
-		double value = 20.02d;
-		BigDecimal bigDecimal = new BigDecimal(value);
-		System.out.println(bigDecimal);
 		return HI + " " + name;
 	}
 }
