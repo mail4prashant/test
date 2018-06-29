a2566ee3451fca560d1f6604dc6bb1a3ce80a9c8
Prashant Pandey <mail4prashant@gmail.com>
2018-06-29 10:22:47 +0200

    failing sonar rules

--- test/src/main/java/com/allianz/test/SayHiService.java
+++ test/src/main/java/com/allianz/test/SayHiService.java
@@ -1,10 +1,15 @@
 package com.allianz.test;
 
+import java.math.BigDecimal;
+
 public class SayHiService {
 
 	private final static String HI = "Hi";
 	
 	public String sayHi(String name) {
+		double value = 20.02d;
+		BigDecimal bigDecimal = new BigDecimal(value);
+		System.out.println(bigDecimal);
 		return HI + " " + name;
 	}
 }
